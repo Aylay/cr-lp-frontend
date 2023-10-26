c247e08fb49037b362024163598c3a2ecedded51
Aylay <luk.tattali@gmail.com>
2023-10-26 10:16:16 +0200

    -

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { page } from "$app/stores";
-	
+
 	import Gtm from '$lib/components/utilities/GTM.svelte';
 	import '../app.css';
 </script>
@@ -11,7 +11,7 @@
 	{/if}
 </svelte:head>
 
-{#if $page.url.host.includes('extreme.fr')}
+{#if $page.url.host.includes('cafesrichard.fr')}
 	<Gtm gtmId="GTM-W3PHBR8" />
 {/if}
 
