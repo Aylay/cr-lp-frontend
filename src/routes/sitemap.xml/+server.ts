caaac0563cd1ade9948ab5f27e695109aec7f43f
Aylay <luk.tattali@gmail.com>
2024-04-16 14:17:38 +0200

    sitemap

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,27 @@
+/** @type {import('./$types').RequestHandler} */
+export async function GET({ setHeaders }) {
+	const siteURL = import.meta.env.VITE_SITE_URL
+	
+	setHeaders({
+		'Content-Type': 'application/xml'
+	});
+	
+	const pages = ['', 'mentions-legales'];
+	
+	const sitemap = `<?xml version="1.0" encoding="UTF-8" ?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+      <url>
+        <loc>${siteURL}</loc>
+      </url>
+      ${pages
+				.map(
+					(page) => `
+			<url>
+				<loc>${siteURL}/${page}</loc>
+			</url>
+			`
+				)
+				.join('')}
+    </urlset>`;
+	return new Response(sitemap);
+}
